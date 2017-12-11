be9242fd390436255b35a5c5cb2bd35b3c8f87da
kaiforward <kaiforward123@gmail.com>
2017-12-11 16:43:54 +0000

    Update base.py

--- settings/base.py
+++ settings/base.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # set static directory in base Directory like this.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "static"),
 ]
 # need static root??
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
